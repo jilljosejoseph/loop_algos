411b1865cb8508e59c4c31423a2bc435af6e5433
Jill Joseph <JJJoseph@my.staugustine.edu>
2020-02-19 11:43:37 -0600

    fizzbuzz

--- 3_fizzbuzz.js
+++ 3_fizzbuzz.js
@@ -14,15 +14,15 @@
 counter = 1
 for (counter = 1; counter <= 30; counter++) {
 
-    if(counter % 3 == 0 && counter % 5 == 0) {
+    if (counter % 3 == 0 && counter % 5 == 0) {
         console.log("FizzBuzz");
     }
-    
-    else if(counter % 3 == 0) {
+
+    else if (counter % 3 == 0) {
         console.log("Fizz");
     }
 
-    else if(counter % 5 == 0) {
+    else if (counter % 5 == 0) {
         console.log("Buzz");
     }
 
